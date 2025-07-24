56ca7a05f734cc68eedad9c63223c4ad58f31431
Andy Teucher <andy.teucher@gmail.com>
2025-07-24 09:05:56 -0600

    fix example proj file names

--- _examples/make-example-project.R
+++ _examples/make-example-project.R
@@ -8,10 +8,10 @@ withr::with_dir("_examples", {
   dir.create("my-project/data/raw_data", showWarnings = FALSE)
   dir.create("my-project/data/derived_data", showWarnings = FALSE)
 
-  file.create("my-project/01-read_data.R")
-  file.create("my-project/02-clean_data.R")
-  file.create("my-project/03-analysis.R")
-  file.create("my-project/04-output.R")
+  file.create("my-project/01_read-data.R")
+  file.create("my-project/02_clean-data.R")
+  file.create("my-project/03_analysis.R")
+  file.create("my-project/04_output.R")
   file.create("my-project/README.md")
 
   file.create("my-project/paper/paper.qmd")
